2f69b9320d2bbae37dc1736510ff1f059bde10a7
Navya1357 <120446779+Navya1357@users.noreply.github.com>
2022-12-13 12:33:54 +0530

    Update constanatsClassVita.java

--- MavenProjectTest/src/test/java/ExcelOperation/constanatsClassVita.java
+++ MavenProjectTest/src/test/java/ExcelOperation/constanatsClassVita.java
@@ -6,6 +6,8 @@ public class constanatsClassVita {
 		   public static final String URL = "https://unicore.azurewebsites.net/account/login";
 		   public static final String TestData_FILEPATH = "C:\\Navya\\TestCasesSample.xlsx";
 		   //public static final String TestData_FILENAME = "LoginTestData.xls";
+	        //public static final String TestData_FILENAME = "LoginTestData.xls";
+
 
 		}
 
